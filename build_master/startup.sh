0986226dce17f7dc74cc8b114da6930170041bc9
Samuele <samuele@busnet.it>
2017-12-14 17:54:13 +0100

    work in progress... quadris

--- build_master/startup.sh
+++ build_master/startup.sh
@@ -5,8 +5,8 @@ echo ${NOMECHAIN}
 
 multichain-util create ${NOMECHAIN}
 
-cp /root/.multichain/${NOMECHAIN}/params2.dat /root/.multichain/${NOMECHAIN}/params.dat
-cp /root/.multichain/${NOMECHAIN}/multichain2.conf /root/.multichain/${NOMECHAIN}/multichain.conf
+cp /root/params.dat /root/.multichain/${NOMECHAIN}/params.dat
+cp /root/multichain.conf /root/.multichain/${NOMECHAIN}/multichain.conf
 
 sed -i -e "s/REPLACE_CHAIN_NAME/${NOMECHAIN}/g" /root/.multichain/${NOMECHAIN}/params.dat
 
